27cc17fcf9e0af1d4203f6247e0ead17c3733f8e
r3dmaohong <r3dmaohong@gmail.com>
2016-08-03 13:53:02 +0800

    upd

--- rscript/employment.circumstance.R
+++ rscript/employment.circumstance.R
@@ -12,6 +12,9 @@ options(java.parameters = "-Xmx4g")
 library(XLConnect)
 library(pbapply)
 library(stringdist)
+library(tmcn)
+library(foreach) 
+library(doSNOW)
 
 ##Import all files.
 files <- list.files(file.path('raw data','就業藍圖所有學校原始資料'),full.names = TRUE)
@@ -64,8 +67,8 @@ nrow(first.job)
 #first.job %>% filter(職務小類名稱!="") %>% nrow()
 #first.job <- first.job %>% filter(職務小類名稱!="")
 
-first.job$學校名稱 <- iconv(first.job$學校名稱,"UTF-8")
-first.job$科系名稱 <- iconv(first.job$科系名稱,"UTF-8")
+first.job$學校名稱 <- toUTF8(first.job$學校名稱) ##iconv(first.job$學校名稱,"UTF-8")
+first.job$科系名稱 <- toUTF8(first.job$科系名稱) ##iconv(first.job$科系名稱,"UTF-8")
 
 ##Correct colleges' name
 uni.college.names <- unique(first.job$學校名稱)
@@ -84,6 +87,7 @@ for(x in 1:length(uni.college.names)){
 ##Create new col to record whether it's error or not.
 first.job$正規化科系名稱 <- ""
 first.job$error <- 0
+first.job <- first.job[!grepl("學分班",first.job$科系名稱),]
 ##Correct departments' name
 unique.college.department <- unique(first.job[,c("學校名稱", "科系名稱")])
 
@@ -113,82 +117,89 @@ for(x in 1:nrow(unique.college.department)){
 ##save.image("D:/abc/wjhong/projects/Graduated.From.University/names.tranfer.complete.RData")
 ##load("D:/abc/wjhong/projects/Graduated.From.University/names.tranfer.complete.RData")
 
-if(no.job==1){
-  num.of.job <- first.job[,c("學校代碼", "學校名稱", "科系名稱", "科系類別代號", 
-                              "科系類別名稱", "產業小類代碼", "產業小類名稱", 
-                              "職務小類代碼", "職務小類名稱")]
-}else{
-  num.of.job <- first.job[,c("學校代碼", "學校名稱", "科系名稱", "科系類別代號", 
-                              "科系類別名稱", 
-                              paste0(c("產業小類代碼", "產業小類名稱", "職務小類代碼",
-                                       "職務小類名稱"),no.job-1))]
-}
-names(num.of.job) <- c("學校代碼", "學校名稱", "科系名稱", "科系類別代號", 
-                       "科系類別名稱", "產業小類代碼", "產業小類名稱", 
-                       "職務小類代碼", "職務小類名稱")
-
-
-first.job %>% filter(職務小類名稱!="") %>% nrow()
-first.job <- first.job %>% filter(職務小類名稱!="")
-
-##Remove data with error. (==1) 
-nrow(first.job[first.job$error==0,])
-first.job = first.job[first.job$error==0,]
-first.job$error = NULL
-
-##Counting job freq in each department of college.
-tmp.first <- first.job[,c("學校名稱", "正規化科系名稱","職務小類名稱")]
-names(tmp.first) <- c("school","department","job")
-tmp.first <- tmp.first %>% group_by(., school, department, job)
-count.first.job <- summarize(tmp.first,count=n()) 
-
-##Combine with old standard file.
-standard.job <- read.csv("就業藍圖基準.csv",stringsAsFactors=F)
-names(standard.job)
-standard.job.first <- standard.job[standard.job$"類別.0.職務..1.產業"==0,c("學校名稱", "科系名稱", "名稱.一.", "樣本數.一.")]
-names(standard.job.first) <- c("school","department","job","count")
-
-##Old names to new names
-for(i in 1:nrow(job.transform)){
-  standard.job.first$job[standard.job.first$job==job.transform$old[i]] <- job.transform$new[i]
-}
-
-count.first.job <- rbind(count.first.job,standard.job.first)
-count.first.job <- count.first.job %>% group_by(., school, department, job)
-count.first.job <- count.first.job[count.first.job$count!="NULL",]
-#write.csv(count.first.job, "合併前查看2.csv",row.names=F)
-count.first.job <- count.first.job %>% mutate(., count=sum(as.numeric(count)))
-
-##Move part-time worker and others to the last of the data frame.
-#count.first.job <- count.first.job %>% arrange(., school, department, -count)
-count.first.job <- unique(count.first.job)
-count.first.job <- count.first.job[order(count.first.job$school,count.first.job$department,-count.first.job$count),]
-count.first.job <- rbind(count.first.job[count.first.job$job!="工讀生" & count.first.job$job!="其他",],count.first.job[count.first.job$job=="工讀生" | count.first.job$job=="其他",])
-
-count.first.job <- count.first.job %>% group_by(school, department) %>% mutate(., percentage=count/sum(count))
-
-##Get each departments of colleges first 10 jobs.
-#count.first.job <- count.first.job %>% group_by(school, department) %>% top_n(n = 10)
-cl <- makeCluster(2) ##2
-registerDoSNOW(cl)
-pb <- txtProgressBar(min = 1, max = nrow(tmp.uni), style = 3)
-print("Output data format processing...")
-tmp.uni <- count.first.job[,c("school", "department")] %>% unique
-dopar.first.job <- foreach (x = 1:nrow(tmp.uni), .combine=rbind) %dopar% {
-  tmp <- head(count.first.job[which(count.first.job$school==tmp.uni$school[x] & count.first.job$department==tmp.uni$department[x]),], 10)
-  if(nrow(tmp)<10){
-    tmp <- rbind(tmp, setNames(data.frame(matrix(rep(c(tmp$school[1], tmp$department[1], NA, NA, NA),11-nrow(tmp)), byrow=T, ncol=5)),names(tmp)))    
+##no.job:1~4 (first job ? ... or the forth one?)   ; type: 0(職務) or 1(產業)
+job_func <- function(no.job,type){
+  if(no.job==1){
+    num.of.job <- first.job[,c("學校代碼", "學校名稱", "正規化科系名稱", "科系類別代號", 
+                               "科系類別名稱", "產業小類代碼", "產業小類名稱", 
+                               "職務小類代碼", "職務小類名稱","error")]
   }else{
-    tmp <- rbind(head(tmp,10), unlist(c(tmp[1,1:2],"其他", (tmp[1,4]/tmp[1,5]*(1-sum(tmp$percentage))), (1-sum(tmp$percentage))))) 
+    num.of.job <- first.job[,c("學校代碼", "學校名稱", "正規化科系名稱", "科系類別代號", 
+                               "科系類別名稱", 
+                               paste0(c("產業小類代碼", "產業小類名稱", "職務小類代碼",
+                                        "職務小類名稱"),no.job-1),"error")]
   }
-  setTxtProgressBar(pb, x) 
-  return(tmp)
+  names(num.of.job) <- c("學校代碼", "學校名稱", "正規化科系名稱", "科系類別代號", 
+                         "科系類別名稱", "產業小類代碼", "產業小類名稱", 
+                         "職務小類代碼", "職務小類名稱","error")
+  
+  
+  num.of.job %>% filter(職務小類名稱!="") %>% nrow()
+  num.of.job <- num.of.job %>% filter(職務小類名稱!="")
+  
+  ##Remove data with error. (==1) 
+  nrow(num.of.job[num.of.job$error==0,])
+  num.of.job = num.of.job[num.of.job$error==0,]
+  num.of.job$error = NULL
+  
+  ##Counting job freq in each department of college.
+  tmp.first <- num.of.job[,c("學校名稱", "正規化科系名稱","職務小類名稱")]
+  names(tmp.first) <- c("school","department","job")
+  tmp.first <- tmp.first %>% group_by(., school, department, job)
+  count.num.of.job <- summarize(tmp.first,count=n()) 
+  
+  ##Combine with old standard file.
+  standard.job <- read.csv("就業藍圖基準.csv",stringsAsFactors=F)
+  names(standard.job)
+  standard.job.first <- standard.job[standard.job$"類別.0.職務..1.產業"==type,c("學校名稱", "科系名稱", paste0(c("名稱", "樣本數"), no.job))]
+  names(standard.job.first) <- c("school","department","job","count")
+  
+  ##Old names to new names
+  for(i in 1:nrow(job.transform)){
+    standard.job.first$job[standard.job.first$job==job.transform$old[i]] <- job.transform$new[i]
+  }
+  
+  count.num.of.job <- rbind(count.num.of.job,standard.job.first)
+  count.num.of.job <- count.num.of.job %>% group_by(., school, department, job)
+  count.num.of.job <- count.num.of.job[count.num.of.job$count!="NULL",]
+  #write.csv(count.num.of.job, "合併前查看2.csv",row.names=F)
+  count.num.of.job <- count.num.of.job %>% mutate(., count=sum(as.numeric(count)))
+  
+  ##Move part-time worker and others to the last of the data frame.
+  #count.num.of.job <- count.num.of.job %>% arrange(., school, department, -count)
+  count.num.of.job <- unique(count.num.of.job)
+  count.num.of.job <- count.num.of.job[order(count.num.of.job$school,count.num.of.job$department,-count.num.of.job$count),]
+  count.num.of.job <- rbind(count.num.of.job[count.num.of.job$job!="工讀生" & count.num.of.job$job!="其他",],count.num.of.job[count.num.of.job$job=="工讀生" | count.num.of.job$job=="其他",])
+  
+  count.num.of.job <- count.num.of.job %>% group_by(school, department) %>% mutate(., percentage=count/sum(count))
+  
+  ##Get each departments of colleges first 10 jobs.
+  #count.num.of.job <- count.num.of.job %>% group_by(school, department) %>% top_n(n = 10)
+  cl <- makeCluster(2) ##2
+  registerDoSNOW(cl)
+  tmp.uni <- count.num.of.job[,c("school", "department")] %>% unique
+  pb <- txtProgressBar(min = 1, max = nrow(tmp.uni), style = 3)
+  print("Output data format processing...")
+  
+  dopar.num.of.job <- foreach (x = 1:nrow(tmp.uni), .combine=rbind) %dopar% {
+    tmp <- head(count.num.of.job[which(count.num.of.job$school==tmp.uni$school[x] & count.num.of.job$department==tmp.uni$department[x]),], 10)
+    if(nrow(tmp)<10){
+      tmp <- rbind(tmp, setNames(data.frame(matrix(rep(c(tmp$school[1], tmp$department[1], NA, NA, NA),11-nrow(tmp)), byrow=T, ncol=5)),names(tmp)))    
+    }else{
+      tmp <- rbind(head(tmp,10), unlist(c(tmp[1,1:2],"其他", (tmp[1,4]/tmp[1,5]*(1-sum(tmp$percentage))), (1-sum(tmp$percentage))))) 
+    }
+    setTxtProgressBar(pb, x) 
+    return(tmp)
+  }
+  close(pb)
+  stopCluster(cl)
+  
+  dopar.num.of.job[which(dopar.num.of.job$job=="工讀生"),c("job", "count", "percentage")] <- c(NA, NA, NA)
+  
+  return(dopar.num.of.job)
 }
-close(pb)
-stopCluster(cl)
 
-dopar.first.job[which(dopar.first.job$job=="工讀生"),c("job", "count", "percentage")] <- c(NA, NA, NA)
 
-##To be continued...
+tmp <- job_func(1)
+write.csv(tmp, "output/各校就業狀況-職務-1.csv",row.names=F)
 
-write.csv(dopar.first.job, "output/各校就業狀況-職務.csv",row.names=F)
