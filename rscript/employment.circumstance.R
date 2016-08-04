1bf1ed1ded3549997063ce1ecb8e9f35ef66d7fa
r3dmaohong <r3dmaohong@gmail.com>
2016-08-04 13:54:12 +0800

    upd

--- rscript/employment.circumstance.R
+++ rscript/employment.circumstance.R
@@ -55,12 +55,12 @@ nrow(total.data)
 college.name.transfer <- read.csv("學校名稱正規化表格.csv",stringsAsFactors=F)
 
 ##First job
-first.job <- total.data[,c("學校代碼", "學校名稱", "科系名稱", "科系類別代號", 
+first.job <- total.data[,c("會員編號","學校代碼", "學校名稱", "科系名稱", "科系類別代號", 
                           "科系類別名稱", "產業小類代碼", "產業小類名稱", 
                           "職務小類代碼", "職務小類名稱", "產業小類代碼1",
-                          "產業小類名稱1", "職務小類代碼1", "職務小類名稱1"
-                          , "產業小類代碼2", "產業小類名稱2", "職務小類代碼2"
-                          , "職務小類名稱2", "產業小類代碼3", "產業小類名稱3", 
+                          "產業小類名稱1", "職務小類代碼1", "職務小類名稱1",
+                          "產業小類代碼2", "產業小類名稱2", "職務小類代碼2",
+                          "職務小類名稱2", "產業小類代碼3", "產業小類名稱3", 
                           "職務小類代碼3", "職務小類名稱3")]
 ##Remove curriculum vitae without first job.
 nrow(first.job)
@@ -91,11 +91,21 @@ first.job <- first.job[!grepl("學分班",first.job$科系名稱),]
 ##Correct departments' name
 unique.college.department <- unique(first.job[,c("學校名稱", "科系名稱")])
 
+##科系正規化依據應為多年科系(如104,105年), 再把舊名稱轉成新的
+tmp <- total.school.lookup.table[,c("學校名稱","X105年科系名稱.藍字.105新增.綠字.改名.橘字.合併.紅字.停招.")]
+tmp2 <- total.school.lookup.table[,c("學校名稱","X104年科系名稱")]
+names(tmp) <- c("學校名稱","科系名稱")
+names(tmp2) <- c("學校名稱","科系名稱")
+department.old2new <- total.school.lookup.table
+total.school.lookup.table <- rbind(tmp, tmp2)
+total.school.lookup.table <- unique(total.school.lookup.table)
+rm(tmp,tmp2)
+
 for(x in 1:nrow(unique.college.department)){
   #jarowinkler('資管',c('資訊管理系','資訊工程學系'))
   ##check if having value in lookup table first.
   department.x <- total.school.lookup.table[which(unique.college.department$學校名稱[x] == total.school.lookup.table$學校名稱)
-                                            ,"科系名稱.藍字.105新增.綠字.改名.橘字.合併.紅字.停招."]
+                                            ,"科系名稱"]
   department.x <- unlist(c(department.x))
   names(department.x)=NULL
   
@@ -112,6 +122,13 @@ for(x in 1:nrow(unique.college.department)){
     print(sprintf("Index %s Error: %s %s", x, unique.college.department$學校名稱[x], unique.college.department$科系名稱[x]))
   }
 }
+##Update old names to new.
+department.old2new <- department.old2new[,c("學校名稱", "X104年科系名稱", "X105年科系名稱.藍字.105新增.綠字.改名.橘字.合併.紅字.停招.")]
+department.old2new <- department.old2new[which(department.old2new[,2]!=department.old2new[,3]),]
+names(department.old2new) <- c("學校名稱", "old", "new")
+for(i in 1:nrow(department.old2new)){
+  first.job$正規化科系名稱[first.job$學校名稱==department.old2new$學校名稱[x] & first.job$科系名稱==department.old2new$old[x]] <- department.old2new$new[x]
+}
 
 ##save the environment.
 ##save.image("D:/abc/wjhong/projects/Graduated.From.University/names.tranfer.complete.RData")
@@ -121,16 +138,16 @@ for(x in 1:nrow(unique.college.department)){
 job_func <- function(no.job,type){  
   gc()
   if(no.job==1){
-    num.of.job <- first.job[,c("學校代碼", "學校名稱", "正規化科系名稱", "科系類別代號", 
+    num.of.job <- first.job[,c("會員編號", "學校代碼", "學校名稱", "正規化科系名稱", "科系類別代號", 
                                "科系類別名稱", "產業小類代碼", "產業小類名稱", 
                                "職務小類代碼", "職務小類名稱","error")]
   }else{
-    num.of.job <- first.job[,c("學校代碼", "學校名稱", "正規化科系名稱", "科系類別代號", 
+    num.of.job <- first.job[,c("會員編號", "學校代碼", "學校名稱", "正規化科系名稱", "科系類別代號", 
                                "科系類別名稱", 
                                paste0(c("產業小類代碼", "產業小類名稱", "職務小類代碼",
                                         "職務小類名稱"),no.job-1),"error")]
   }
-  names(num.of.job) <- c("學校代碼", "學校名稱", "正規化科系名稱", "科系類別代號", 
+  names(num.of.job) <- c("會員編號", "學校代碼", "學校名稱", "正規化科系名稱", "科系類別代號", 
                          "科系類別名稱", "產業小類代碼", "產業小類名稱", 
                          "職務小類代碼", "職務小類名稱","error")
   
@@ -148,6 +165,7 @@ job_func <- function(no.job,type){
   nrow(num.of.job[num.of.job$error==0,])
   num.of.job = num.of.job[num.of.job$error==0,]
   num.of.job$error = NULL
+  num.of.job <- setDF(unique(setDT(num.of.job)))
   
   ##Counting job freq in each department of college.
   tmp.first <- num.of.job[,c("學校名稱", "正規化科系名稱",col2p)]
@@ -158,7 +176,7 @@ job_func <- function(no.job,type){
   ##Combine with old standard file.
   standard.job <- read.csv("就業藍圖基準.csv",stringsAsFactors=F)
   names(standard.job)
-  standard.job.first <- standard.job[standard.job$"類別.0.職務..1.產業"==type,c("學校名稱", "科系名稱", paste0(c("名稱", "樣本數"), no.job))]
+  standard.job.first <- standard.job[standard.job$"類別.0.職務..1.產業."==type,c("學校名稱", "科系名稱", paste0(c("名稱", "樣本數"), no.job))]
   names(standard.job.first) <- c("school","department","job","count")
   
   ##Old names to new names
@@ -213,4 +231,5 @@ total.output.lst <- list()
 for(x in 1:4){
   total.output.lst[[x]] <- job_func(x,0)
 }
+write.csv(total.output.lst[[1]],"test.csv",row.names=F)
 
